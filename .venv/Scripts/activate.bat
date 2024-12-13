769e6a6767a5862d763aa257b2853356ca77fa4a
hakimj <justine.hakim@edu.esiee.fr>
2024-12-13 16:01:59 +0100

    cleaning the data E22

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=U:\E3\Python\projet-dashboard-python\.venv
+set VIRTUAL_ENV=U:\Lina-Tefiniaina-Dashboard-Python\projet-dashboard-python\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
