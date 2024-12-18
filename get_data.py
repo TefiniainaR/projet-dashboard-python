6e9ec0e78327a06c6af2f4a808efd197522fd214
hakimj <justine.hakim@edu.esiee.fr>
2024-12-18 13:40:49 +0100

    Add relative path

--- get_data.py
+++ get_data.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-file_path = r'U:\Lina-Tefiniaina-Dashboard-Python\projet-dashboard-python\data\raw\rawdata.csv'
+file_path = r'data\raw\rawdata.csv'
 def get_data_from_csv(file_path):
     # Charger le fichier CSV dans un DataFrame pandas
     df = pd.read_csv(file_path, sep=',', encoding='utf-8')
